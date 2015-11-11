b7e8229f7f820ff40cfd43971972e0f50478eba5
herve <herve@02247062-64a4-46ef-9812-58f57a7e1da3>
2015-11-11 23:49:08 +0000

    lib-py: record.py: wip, tests still failing
    git-svn-id: svn://svn.saintamh.org/code/lib-python/trunk/saintamh/record@2911 02247062-64a4-46ef-9812-58f57a7e1da3

---
+++ shortcuts.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+$Id: $
+Herve Saint-Amand
+Edinburgh
+"""
+
+#----------------------------------------------------------------------------------------------------------------------------------
+# includes
+
+# this module
+from .record import Field, compile_field_def
+
+#----------------------------------------------------------------------------------------------------------------------------------
+
+def value_check (name, check):
+    def func (fdef):
+        fdef = compile_field_def(fdef)
+        if fdef.check is not None:
+            raise Exception ("I haven't figured out how to chain value checks yet")
+        return fdef.derive (check=check)
+    func.__name__ = name
+    return func
+
+nonnegative = value_check ('nonnegative', '{} >= 0')
+strictly_positive = value_check ('strictly_positive', '{} > 0')
+
+#----------------------------------------------------------------------------------------------------------------------------------
+
+def regex_check (name, char_def):
+    def func (n=None):
+        multiplier = ('{{{{%d}}}}' % n) if n is not None else '*'
+        fdef = Field (
+            type = str,
+            check = "re.search(r'^[%s]%s$',{})" % (char_def, multiplier),
+        )
+        fdef.ns.add (re)
+        return fdef
+    func.__name__ = name
+    return func
+
+uppercase_letters = regex_check ('uppercase_letters', 'A-Z')
+uppercase_wchars  = regex_check ('uppercase_wchars', '[A-Z0-9_]')
+uppercase_hex     = regex_check ('uppercase_hex', '0-9A-F')
+
+lowercase_letters = regex_check ('lowercase_letters', 'a-z')
+lowercase_wchars  = regex_check ('lowercase_wchars', '[a-z0-9_]')
+lowercase_hex     = regex_check ('lowercase_hex', '0-9a-f')
+
+digits_str        = regex_check ('digits_str', '0-9')
+
+absolute_http_url = Field (
+    type = str,
+    check = "re.search(r'^https?://.{1,2000}$',{})"
+)
+
+#----------------------------------------------------------------------------------------------------------------------------------
---
+++ unpickler.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+$Id: $
+Herve Saint-Amand
+Edinburgh
+"""
+
+#----------------------------------------------------------------------------------------------------------------------------------
+
+# Because Records are dynamically created classes that are compiled within a function, 'pickle' cannot find the class definition by
+# name alone. For this reason we need to keep a register here of all Record classes that have been created, indexed by name.
+#
+# This is rather hacky, and has a few implications: you shouldn't create millions of record classes, as they'll all be referenced
+# here, and you can't create two record classes with the same name. The latter could come back to bite me some day. I'm not sure
+# what I'll do then.
+
+ALL_RECORDS = {}
+
+def register_record_class_for_unpickler (cls_name, cls):
+    ALL_RECORDS[cls_name] = cls
+
+class RecordUnpickler (object):
+    def __init__ (self, cls_name):
+        self.cls_name = cls_name
+    def __call__ (self, *values):
+        return ALL_RECORDS[self.cls_name](*values)
+
+#----------------------------------------------------------------------------------------------------------------------------------
