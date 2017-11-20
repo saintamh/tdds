e6e80d2b4e19732351e9be48f9ec1b83ab3c61be
Hervé Saint-Amand <git@saintamh.org>
2017-11-20 15:23:49 +0000

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+#----------------------------------------------------------------------------------------------------------------------------------
+
+from __future__ import unicode_literals
+
+from distutils.core import setup
+
+#----------------------------------------------------------------------------------------------------------------------------------
+
+setup(
+    name='Record',
+    version='0.1',
+    description='Immutable type-checked data structures',
+    author='Herv\u00e9 Saint-Amand',
+    author_email='record@saintamh.org',
+    url='https://saintamh.org/code/record/',
+    packages=['record'],
+)
+
+#----------------------------------------------------------------------------------------------------------------------------------
