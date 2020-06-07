8c4d79500bc6d7eef8f5e497db72ed3e0f92d92f
Herve Saint-Amand <git@saintamh.org>
2020-06-07 07:31:30 +0100

    Remove Python2 support
    Python2 doesn't have the latest pylint, and it's getting messy to support both. Python2 is past EOL anyway.

--- setup.py
+++ setup.py
@@ -28,7 +28,6 @@ setuptools.setup(
     classifiers=[
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Topic :: Software Development :: Libraries',
     ],
